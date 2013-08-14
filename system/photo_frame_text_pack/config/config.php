637018884e3f164c888f79f6066076d60a57849e
Justin Kimbrell <justin@objectivehtml.com>
2013-08-14 11:33:24 -0400

    Added config file

---
+++ system/photo_frame_text_pack/config/config.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['photo_frame_text_pack_version'] = '1.0.0';
+
+if(!defined('PHOTO_FRAME_TEXT_PACK_VERSION'))
+{
+	define('PHOTO_FRAME_TEXT_PACK_VERSION', $config['photo_frame_text_pack_version']);
+}
